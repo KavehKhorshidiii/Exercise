2bc1d9e6ea33f2d561b4ffe0c25a511e74de84fd
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-02 23:39:08 +0330

    commit

--- app/template/homePage_template/homeCard/HomeCards.tsx
+++ app/template/homePage_template/homeCard/HomeCards.tsx
@@ -7,8 +7,10 @@ export default function TheHomeCards() {
 
     <div className='grid my-5 content-center gap-10 justify-items-center grid-cols-3 w-10/12'>
 
+
       {HomeData.homes.map(item => { return <Card details={item} key={item.id}/>})}
-      
+
+
     </div>
 
   )
