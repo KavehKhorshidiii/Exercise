edc775a678fb7a44488146580a9cbd9f67ae1623
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-02 23:38:39 +0330

    commit

--- app/modules/Card/card.tsx
+++ app/modules/Card/card.tsx
@@ -15,13 +15,13 @@ type homeDetailsType = {
 export default function card({details}:homeDetailsType) {
   return (
 
-      <div className=' w-96 h-[400px] bg-gray-400 overflow-hidden rounded-4xl border-4'>
+      <div className=' w-96 h-96 bg-gray-400 overflow-hidden rounded-4xl border-4'>
         <div className=' h-40 bg-white '></div>
         <p>{details.title}</p>
         <p>roomCount: {details.roomCount}</p>
         <p>meterage: {details.meterage}</p>
         <p>price: {details.price}</p>
       </div>
-      
+
   )
 }
