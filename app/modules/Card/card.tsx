e82ede132492ef07b58eaeb2a66d32d37be5bf4c
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-03 12:55:40 +0330

    commit

--- app/modules/Card/card.tsx
+++ app/modules/Card/card.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import Image from 'next/image'
+import Link from "next/link"
 
 type homeDetailsType = {
   details:{
@@ -17,18 +16,18 @@ export default function card({details}:homeDetailsType) {
 
       <div className=' w-96 h-96 bg-gray-400 overflow-hidden rounded-4xl border-4'>
 
-        <div className='Image h-2/3 bg-white '></div>
+        <div className='Image h-1/2 bg-white '></div>
 
-        <div className=' flex h-1/3 flex-col justify-around'>
+        <div className=' flex h-1/2 flex-col justify-between'>
           <div>
             <p>{details.title}</p>
             <p>roomCount: {details.roomCount}</p>
             <p>meterage: {details.meterage}</p>
             <p>price: {details.price}</p>
           </div>
-          <div>
-            <button className=' border-2 bg-amber-600 w-full '>مشاهده ملک</button>
-          </div>
+         
+            <Link href={`/homeStore/${details.id}`} className='py-5 text-center bg-yellow-600 w-full '>مشاهده ملک</Link>
+         
         </div>
 
       </div>
