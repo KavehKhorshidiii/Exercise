956e5edf3e21b03344c1d4cdc0f496271726eb61
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-05 23:50:37 +0330

    commit

--- app/template/homePage_template/Features/Features.tsx
+++ app/template/homePage_template/Features/Features.tsx
@@ -1,19 +1,46 @@
 "use client"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import dataHomes from "@/data/data.json"
+import Card from '@/app/modules/Card/card'
+
+type HomeStateType = {
+    id:number;
+    title:string;
+    img:string;
+    roomCount:number;
+    meterage:number;
+    price:number
+}
 
 export default function TheFeatures() {
 
   const [search , setSearch] = useState("")
-  const [homes , setHomes] = useState([])
+  const [homes , setHomes] = useState<HomeStateType[]>([])
   
-  const hhh = dataHomes.homes.filter((item)=> item.title.includes(search))
-  console.log(hhh)
+  const homeFilter = dataHomes.homes.filter((item)=> item.title.includes(search))
+
+
+  const runSearchFunc = () => {
+    setHomes([...homeFilter])
+    console.log(homeFilter)
+
+  }
+
+  useEffect(()=>{},[])
 
   return (
+
     <div className=' bg-gray-900 w-full h-96'>
+      
+      
+      <div className='grid my-5 border-red-600 border-8 content-center gap-10 justify-items-center grid-cols-3 w-10/12'>
+      { 
+        homes.map(item => { return <Card details={item} key={item.id}/>})
+      }
+      </div>
       <h1 className=' text-white'>{search}</h1>
       <input onChange={(e)=> setSearch(e.target.value)} className=' text-black border-white bg-amber-100 h-10 w-60 rounded-2xl' type="search" name="" id="" />
+      <button onClick={runSearchFunc}>search</button>
     </div>
   )
 }
--- app/template/homePage_template/homeCard/HomeCards.tsx
+++ app/template/homePage_template/homeCard/HomeCards.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 import HomeData from '@/data/data.json'
 import Card from '@/app/modules/Card/card'
+import { Feature } from 'next/dist/build/webpack/plugins/telemetry-plugin/telemetry-plugin'
+
+
 
 export default function TheHomeCards() {
 
@@ -7,9 +12,7 @@ export default function TheHomeCards() {
 
     <div className='grid my-5 content-center gap-10 justify-items-center grid-cols-3 w-10/12'>
 
-
       {HomeData.homes.map(item => { return <Card details={item} key={item.id}/>})}
-
 
     </div>
 
