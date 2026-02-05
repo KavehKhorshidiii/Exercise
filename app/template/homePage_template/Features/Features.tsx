f7330b6067227045d463c11594cc0f928f74b227
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-05 19:23:26 +0330

    commit

--- app/template/homePage_template/Features/Features.tsx
+++ app/template/homePage_template/Features/Features.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+"use client"
+import { useState } from 'react'
 
-export default function theFeatures() {
+export default function TheFeatures() {
+
+  const [search , setSearch] = useState("")
+
   return (
-    <div>
-      theFeatures
+    <div className=' bg-gray-900 w-full h-96'>
+      <h1 className=' text-white'>{search}</h1>
+      <input onChange={(e)=> setSearch(e.target.value)} className=' text-black border-white bg-amber-100 h-10 w-60 rounded-2xl' type="search" name="" id="" />
     </div>
   )
 }
+
