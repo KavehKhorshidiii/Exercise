0c2e655f51d0ae7cb412bfb8fef2ce7a64186135
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-06 13:09:35 +0330

    commit

--- app/template/homePage_template/Features/Features.tsx
+++ app/template/homePage_template/Features/Features.tsx
@@ -1,50 +1,41 @@
 "use client"
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import dataHomes from "@/data/data.json"
 import Card from '@/app/modules/Card/card'
 
 type HomeStateType = {
-    id:number;
-    title:string;
-    img:string;
-    roomCount:number;
-    meterage:number;
-    price:number
+  id: number;
+  title: string;
+  img: string;
+  roomCount: number;
+  meterage: number;
+  price: number
 }
 
 export default function TheFeatures() {
 
-  const [search , setSearch] = useState("")
-  const [homes , setHomes] = useState<HomeStateType[]>([])
-  
-  const homeFilter = dataHomes.homes.filter((item)=> item.title.includes(search))
+  const [search, setSearch] = useState("")
+  const [homes, setHomes] = useState<HomeStateType[]>([...dataHomes.homes])
 
- 
+  const homeFilter = dataHomes.homes.filter((item) => item.title.includes(search))
 
-  {useEffect(()=>{setHomes(...dataHomes)},[])}
-
-  const runSearchFunc = () => {
+  const runChangeFunc = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value)
     setHomes([...homeFilter])
-    console.log(homeFilter)
-
   }
 
-  console.log(homes)
-  
   return (
-    
-    <div className=' bg-gray-900 w-full h-96'>
-      
-      
-      <div className='grid my-5 border-red-600 border-8 content-center gap-10 justify-items-center grid-cols-3 w-10/12'>
-      { 
-        homes.map(item => { return <Card details={item} key={item.id}/>})
-      }
+
+    <div className='  my-5 content-center gap-10 justify-items-center grid-cols-3  w-full'>
+      <div className=' grid my-5 content-center gap-10 justify-items-center grid-cols-3 w-10/12'>
+        {
+          homes.map(item => { return <Card details={item} key={item.id} /> })
+        }
       </div>
       <h1 className=' text-white'>{search}</h1>
-      <input onChange={(e)=> setSearch(e.target.value)} className=' text-black border-white bg-amber-100 h-10 w-60 rounded-2xl' type="search" name="" id="" />
-      <button onClick={runSearchFunc}>search</button>
+      <input onChange={(e) => runChangeFunc(e)} className=' text-black border-black border-4 bg-amber-100 h-10 w-60 rounded-2xl' type="search" name="" id="" />
     </div>
   )
+
 }
 
