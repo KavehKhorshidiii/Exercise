081a99b13a96f0bda9643409ea576bd77b46d8c9
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-06 14:49:02 +0330

    commit

--- app/template/homePage_template/Features/Features.tsx
+++ app/template/homePage_template/Features/Features.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { ChangeEvent, useEffect, useState } from 'react'
+import React, { ButtonHTMLAttributes, ChangeEvent, InputEvent, ReactElement, useEffect, useState } from 'react'
 import dataHomes from "@/data/data.json"
 import Card from '@/app/modules/Card/card'
 
@@ -14,29 +14,31 @@ type HomeStateType = {
 
 export default function TheFeatures() {
 
-  const [search, setSearch] = useState("")
   const [homes, setHomes] = useState<HomeStateType[]>([...dataHomes.homes])
-
-  useEffect(()=>{
-
-    if(homes.length === 0 ){
-      setHomes([...dataHomes.homes])
-    }
-
-  },[search , ...dataHomes.homes])
+  const [search, setSearch] = useState("")
   
-
   const homeFilter = dataHomes.homes.filter((item) => item.title.includes(search))
-
-  const runChangeFunc = (e: ChangeEvent<HTMLInputElement>) => {
+  
+  
+  const runChangeFunc = (e:React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value)
+  }
+  
+  const runBtnFunc = () => {
     setHomes([...homeFilter])
   }
+
+
+
+
 
   return (
 
     <div className='  my-5 content-center gap-10 justify-items-center grid-cols-3  w-full'>
-      <input onChange={(e) => runChangeFunc(e)} className=' text-black border-black border-4 bg-amber-100 h-10 w-60 rounded-2xl' type="search" name="" id="" />
+      <div className='w-10/12 m-5 flex gap-2 '>
+        <input onChange={(e)=>runChangeFunc(e)} className=' text-black border-black border-2 bg-white h-10 w-60 rounded-2xl' type="search"/>
+        <button onClick={runBtnFunc} className=' text-black border-black border-2 bg-white h-10 px-4 rounded-2xl'>Search</button>
+      </div>
       <div className=' grid my-5 content-center gap-10 justify-items-center grid-cols-3 w-10/12'>
         {
           homes.map(item => { return <Card details={item} key={item.id} /> })
