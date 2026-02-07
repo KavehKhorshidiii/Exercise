36b00dcfe481bfa4e3c90a464eee2c2396803a66
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-07 11:05:41 +0330

    commit

--- app/template/homePage_template/Features/Features.tsx
+++ app/template/homePage_template/Features/Features.tsx
@@ -14,55 +14,57 @@ type HomeStateType = {
 
 export default function TheFeatures() {
 
-  const [countPost, setCountPost] = useState(6)
-  const [pagination, setPagination] = useState(0)
-  const [countBtnPagination, setCountBtnPagination] = useState(Math.ceil( dataHomes.homes.length / countPost ))
 
+  //تعداد پست های که باید نمایش بده
+  const [countPost, setCountPost] = useState(3)
+  // صفحه ای که روی اون هستیم
+  const [pagination, setPagination] = useState(1)
+  // تعداد دکمه های
+  const countBtnPagination = Math.ceil(dataHomes.homes.length / countPost)
 
-  const [homes, setHomes] = useState<HomeStateType[]>([...dataHomes.homes].slice(pagination , countPost))
+  const endLength = pagination * countPost
+  const startLength = endLength - countPost
+
   const [search, setSearch] = useState("")
-
-   
-
-  console.log("pagination:" ,  pagination)
-
-
-
   
 
+  const [homeFilter , setHomeFilter] = useState( [...dataHomes.homes].filter((item)=> item.title.includes(search)))
+  
 
-  const homeFilter = dataHomes.homes.filter((item) => item.title.includes(search))
+  // نشان دهنده پست ها
+  const homes = homeFilter.slice(startLength, endLength)
 
-  const runChangeFunc = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value)
-  }
-  const runBtnFunc = () => {
-    setHomes([...homeFilter])
-  }
+
+
+
+
   const runChangeOption = (e: ChangeEvent<HTMLSelectElement>) => {
     switch (e.target.value) {
       case "price": {
-        const sortPrice = [...dataHomes.homes].sort((A, B) => A.price - B.price)
-        setHomes([...sortPrice])
+        //homeFilter.sort((A, B) => A.price - B.price) 
+        console.log(e.target.value)
         break
       }
       case "room": {
-        const sortRoom = [...dataHomes.homes].sort((A, B) => A.roomCount - B.roomCount)
-        setHomes([...sortRoom])
+        //return [...homeFilter].sort((A, B) => A.roomCount - B.roomCount)
+        console.log(e.target.value)
         break
       }
       case "size": {
-        const sortSize = [...dataHomes.homes].sort((A, B) => A.meterage - B.meterage)
-        setHomes([...sortSize])
+        //return [...homeFilter].sort((A, B) => A.meterage - B.meterage)
+        console.log(e.target.value)
         break
       }
-      default: {
-        setHomes([...dataHomes.homes])
-      }
+      
     }
   }
 
 
+
+
+  const fun = (num: number) => {
+    setPagination(num + 1)
+  }
 
 
   return (
@@ -71,8 +73,7 @@ export default function TheFeatures() {
 
       <div className='w-10/12 m-5 justify-between flex gap-2 '>
         <div>
-          <input onChange={(e) => runChangeFunc(e)} className=' text-black border-black border-2 bg-white h-10 w-60 rounded-2xl' type="search" />
-          <button onClick={runBtnFunc} className=' ml-1 text-black border-black border-2 bg-white h-10 px-4 rounded-2xl'>Search</button>
+          <input onChange={(e) => setSearch(e.target.value)} className=' text-black border-black border-2 bg-white h-10 w-60 rounded-2xl' type="search" />
         </div>
         <div>
           <select onChange={runChangeOption} className='text-black border-black border-2 bg-white h-10 w-60 rounded-2xl' name="a" id="">
@@ -83,19 +84,22 @@ export default function TheFeatures() {
           </select>
         </div>
       </div>
+
       <div className=' grid my-5 content-center gap-10 justify-items-center grid-cols-3 w-10/12'>
         {
           homes.map(item => { return <Card details={item} key={item.id} /> })
         }
       </div>
 
-
       <ul className=" flex  gap-2 border-2 p-5 rounded-2xl text-black">
         {
-          Array.from({length:countBtnPagination}).map((item , index) => <li key={index} className=" flex justify-center items-center border-2 rounded-full size-12 "><button onClick={()=>setPagination(index)}> {index + 1} </button></li>)
+          Array.from({ length: countBtnPagination }).map((item, index) =>
+            <li onClick={() => fun(index)} key={index} className=" flex justify-center items-center border-2 rounded-full size-12 "> {index + 1} </li>
+          )
         }
         <li className=" justify-center content-center"><a href="#"> next </a></li>
       </ul>
+
     </div>
   )
 
