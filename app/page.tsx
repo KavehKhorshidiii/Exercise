8ccc2af5df55c6a53472804d0b13438437428e8b
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-08 12:10:37 +0330

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
+
+async function Api() {
+  const res = await fetch('https://fakestoreapi.com/products')
+  
+  if (!res.ok) {
+    console.log("Error")
+  }
+
+  return res.json()
+}
+
+type ProductsType = {
+  category:string,
+  description:string,
+  id:number,
+  image:string,
+  price:number,
+  rating:{ rate:number, count:number },
+  title:string
+}
+
+export default async function Home() {
+
+  const ApiFunc:ProductsType[] = await Api()
+
   return (
     <div className="bg-black flex flex-col justify-center items-center">
-      Test
+      {
+        ApiFunc.map((item:ProductsType) => <p key={item.id}>{item.title}</p>)
+      }
     </div>
   );
 }
