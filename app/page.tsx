757d5914551e61badadc5f25ba9a22ade2b709c4
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-08 16:39:43 +0330

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,6 @@ export default async function Home() {
   return (
     <div className="bg-black flex flex-col justify-center items-center">
       {
-
         ApiFunc.map((item:ProductsType) => <p key={item.id}>{item.title}</p>)
 
       }
