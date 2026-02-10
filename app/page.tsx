39c4827ab5f36b6f8832a13e23261a2a4a0b68b5
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-10 21:33:12 +0330

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 export default async function Home() {
   return (
     <div className="bg-black text-3xl flex flex-col justify-center items-center">
-      
-      Kaveh Khorshidi
 
+      Kaveh Khorshidi
     </div>
   );
 }
