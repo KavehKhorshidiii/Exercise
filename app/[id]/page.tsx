13a5a2d97b923d5b9c4830bc46aa4da477f7e9e6
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-12 18:21:08 +0330

    commit

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,12 +1,54 @@
-'use client'
+// "use client";
+// import { useSearchParams } from "next/navigation";
+// //? => http://localhost:3000/lolo?color=red&size=xl
+// export default function SearchDemo() {
+//   const searchParams = useSearchParams();
+//   const color = searchParams.get("color");
+//   const size = searchParams.get("size");
+//   return (
+//     <div>
+//       <p>رنگ: {color}</p> 
+//       {/* => red */}
+//       <p>سایز: {size}</p> 
+//       {/* => xl */}
+//     </div>
+//   );
+// }
 
 
-import { useParams } from "next/navigation";
 
-export default function ProductPage() {
-  const params = useParams(); // دسترسی به پارامتر داینامیک [id]
-  
+"use client";
+
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+
+export default function FilterButton() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  console.log('router =>' , router)
+  console.log('pathname => ' , pathname)
+  console.log('searchParams => ' , searchParams)
+
+  const handleClick = () => {
+    // ساخت query جدید
+    const newQuery = new URLSearchParams(searchParams.toString());
+    newQuery.set("color", "blue");
+    newQuery.set("size", "l");
+
+    // ترکیب pathname و query → URL کامل
+    const url = `${pathname}?${newQuery.toString()}`;
+
+    router.push(url); // هدایت بدون رفرش
+  };
+
   return (
-      <h1>product with ID = {params.id}</h1>
-  )
+        <>
+            <p>color = {searchParams.get("color")}</p>
+            <p>color = {searchParams.get("size")}</p>
+            
+            <button onClick={handleClick}>اعمال فیلتر جدید</button>
+        </>
+  ) 
 }
+
