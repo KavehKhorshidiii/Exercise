ea308927120d29324fbb40eaacf5d477690e3409
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-13 21:22:56 +0330

    commit

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -9,9 +9,10 @@ export default function FilterButton() {
   const searchParams = useSearchParams(); // هوک useSearchParams برای دسترسی به پارامترهای جستجو (query parameters)
 
 
-  console.log('router =>' , router)
+  //console.log('router =>' , router)
   console.log('pathname => ' , pathname)
   console.log('searchParams => ' , searchParams)
+
 
 
   const handleClick = () => {
