e9982bfd37b57b4819c967f8fe981b6d2144df87
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-14 13:06:07 +0330

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div className="bg-black text-3xl flex flex-col justify-center items-center">
       Kaveh Khorshidi
-      <Link href={{pathname:'/newPage',query:{name:"gogo"}}}>newPage</Link>
     </div>
   );
 }
