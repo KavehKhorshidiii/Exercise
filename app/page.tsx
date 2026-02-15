4cf6789f365bffbbd31b5bb4a7d97459d49ed1a3
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-15 22:41:38 +0330

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ export default function Home() {
 
   return (
     <div className="bg-black text-3xl flex flex-col justify-center items-center">
-      
-      Kaveh Khorshidi
 
+      Kaveh Khorshidi
     </div>
   );
 
