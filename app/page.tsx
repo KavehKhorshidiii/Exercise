9ab569316376916aa7e35da5c416bde161915bb2
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-18 20:26:31 +0330

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,13 @@ export default async function Home() {
   const api = await fetch('https://jsonplaceholder.typicode.com/users')
   const users = await api.json()
 
+
   return (
     <div className="bg-black  flex flex-col justify-center items-center">
       {
         users.map((item:UserType) => (
           <p key={item.id}>{item.name}</p>
-        ))
+        ))        
       }
     </div>
   );
