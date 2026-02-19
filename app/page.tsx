f3431fafee23dcfaf5f1dd4bf546470073ae0c09
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-19 21:55:19 +0330

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   }
 
 
-  const api= await fetch('https://jsonplaceholder.typicode.com/users')
+  const api = await fetch('https://jsonplaceholder.typicode.com/users')
   console.log(api)
   const users = await api.json()
 
