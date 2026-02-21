30bb2c49e4e6e96e20b5c3608c7176e719316de9
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-21 12:23:29 +0330

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,8 @@ type UserType = {
 }
 
 export default async function Home() {
-  const res = await fetch(
-    "https://jsonplaceholder.typicode.com/users"
-  )
+  
+  const res = await fetch("https://jsonplaceholder.typicode.com/users")
 
   const users = await res.json()
 
