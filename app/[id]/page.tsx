d9ca744cb8682b8cd423d2a1255014338fc86943
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-24 23:54:28 +0330

    commit

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -7,6 +7,7 @@ export default function UserPage() {
 
     const [x , setX] = useState(true)
 
+    
     return (
         <div>
            <button onClick={()=> setX(!x)} className=" border-2 p-4 rounded-2xl">change</button>
