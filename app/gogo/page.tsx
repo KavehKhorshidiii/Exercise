083377bb5bd9c105aa9a727c8f9a01047bb29255
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-25 13:17:43 +0330

    commit

--- app/gogo/page.tsx
+++ app/gogo/page.tsx
@@ -3,8 +3,8 @@ import { useSearchParams } from "next/navigation"
 
 
 export default function FilterInfo() {
-  const searchParams = useSearchParams()
 
+  const searchParams = useSearchParams()
   const category = searchParams.get("category")
 
   return <p>Category: {category}</p>
