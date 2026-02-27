4bff54750675248077fed702f0cc267600d6b6d7
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2026-02-27 22:12:40 +0330

    commit

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -2,7 +2,9 @@
 import { useSearchParams } from "next/navigation";
 
 export default function Demo() {
+  
   const searchParams = useSearchParams();
 
   return <p>{searchParams.get("color")}</p>;
+  
 }
